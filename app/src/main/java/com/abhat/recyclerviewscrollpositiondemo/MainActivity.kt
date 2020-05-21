d10478e7eb64ec98b7d9ba5fd345977f4ea6c677
Anirudh <anirudh@Anirudhs-MacBook-Pro.local>
2020-05-21 17:52:33 +0530

    Initial commit

--- app/src/main/java/com/abhat/recyclerviewscrollpositiondemo/MainActivity.kt
+++ app/src/main/java/com/abhat/recyclerviewscrollpositiondemo/MainActivity.kt
@@ -1,22 +1,68 @@
 package com.abhat.recyclerviewscrollpositiondemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainScope = CoroutineScope(Dispatchers.Main + SupervisorJob())
+    private val ioScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+    private val recyclerAdapter by lazy {
+        RecyclerAdapter(listOf())
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupRecyclerView()
+        showProgressBar()
+        doSomeDummyNetworkCallUsingDelay()
+    }
+
+    private fun showProgressBar() {
+        progress_bar.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressBar() {
+        progress_bar.visibility = View.GONE
+    }
+
+    private fun doSomeDummyNetworkCallUsingDelay() {
+        CoroutineScope(mainScope.coroutineContext).launch {
+            supervisorScope {
+                dummyNetworkCall()
+            }
+        }
+    }
+
+    private suspend fun dummyNetworkCall() {
+        supervisorScope {
+            async {
+                delay(2000)
+            }.await()
+            hideProgressBar()
+            recyclerAdapter.updateList(populateAndReturnList())
+            recyclerAdapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun populateAndReturnList(): List<String> {
+        var position = 0
+        val demoList = mutableListOf<String>()
+        repeat(50) {
+            demoList.add("Title $position")
+        }
+        return demoList
     }
 
     private fun setupRecyclerView() {
         rv_title.layoutManager = LinearLayoutManager(this)
-        rv_title.adapter = RecyclerAdapter()
+        rv_title.adapter = recyclerAdapter
         rv_title.adapter?.stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
     }
 }
--- app/src/main/java/com/abhat/recyclerviewscrollpositiondemo/RecyclerAdapter.kt
+++ app/src/main/java/com/abhat/recyclerviewscrollpositiondemo/RecyclerAdapter.kt
@@ -9,13 +9,13 @@ import kotlinx.android.synthetic.main.item_single_row.view.*
 /**
  * Created by Anirudh Uppunda on 21,May,2020
  */
-class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.DemoViewHolder>() {
+class RecyclerAdapter(private var demoList: List<String>): RecyclerView.Adapter<RecyclerAdapter.DemoViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DemoViewHolder {
         return DemoViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_single_row, parent, false))
     }
 
     override fun getItemCount(): Int {
-        return 50
+        return demoList.size
     }
 
     override fun onBindViewHolder(holder: DemoViewHolder, position: Int) {
@@ -28,5 +28,9 @@ class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.DemoViewHolder>() {
                 tv_title.text = "Title $position"
             }
         }
+    }
+
+    fun updateList(demoList: List<String>) {
+        this.demoList = demoList
     }
 }
